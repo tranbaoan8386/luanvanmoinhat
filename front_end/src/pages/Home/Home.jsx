41f0736e111ea17266c9b6fac301f941bb658036
tranbaoan8386 <tranbaoan14082003@gmail.com>
2026-02-27 10:44:29 +0700

    a

--- front_end/src/pages/Home/Home.jsx
+++ front_end/src/pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import { Box, Container } from '@mui/material'
-import React from 'react'
-import ProductList from './modules/ProductList'
-import Slider from './modules/Slider'
+import { Box, Container } from "@mui/material";
+import React from "react";
+import ProductList from "./modules/ProductList";
+import Slider from "./modules/Slider";
 
 export default function Home() {
   return (
     <Box sx={{ mb: 3 }}>
       <Slider />
-      <Container maxWidth="xl"> {/*  Giới hạn lại không quá to */}
+      <Container maxWidth="xl">
+        {" "}
+        {/*  Giới hạn lại không quá to */}
         <ProductList />
       </Container>
     </Box>
-  )
+  );
 }
